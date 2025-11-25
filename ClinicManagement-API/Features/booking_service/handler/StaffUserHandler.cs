b6e55731e16a9dfa6d4990e680dd681d71d9c004
khoa <khoadd879@gmail.com>
2025-11-26 00:00:02 +0700

    stash

---
+++ ClinicManagement-API/Features/booking_service/handler/StaffUserHandler.cs
@@ -0,0 +1,6 @@
+namespace ClinicManagement_API.Features.booking_service.handler;
+
+public class StaffUserHandler
+{
+    
+}
---
+++ ClinicManagement-API/Features/booking_service/service/StaffUserService.cs
@@ -0,0 +1,6 @@
+namespace ClinicManagement_API.Features.booking_service.service;
+
+public class StaffUserService
+{
+    
+}

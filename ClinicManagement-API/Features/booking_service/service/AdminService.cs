9b48cb24fd74cb9a7254856da943bd30247e1466
minkhoaa <tukhoa040505@gmail.com>
2025-11-29 01:39:35 +0700

    update: adminservice

--- ClinicManagement-API/Features/booking_service/service/AdminService.cs
+++ ClinicManagement-API/Features/booking_service/service/AdminService.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ClinicManagement_API.Infrastructure.Persisstence;
 
 namespace ClinicManagement_API.Features.booking_service.service
